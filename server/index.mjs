789fd9a9f79b54f200acea1aec6a6cbef543f8b3
Hoàng Như Nghĩa BAT Tech <nghiahn@batgroup.vn>
2023-01-17 10:05:37 +0700

    update

--- server/index.mjs
+++ server/index.mjs
@@ -5,19 +5,32 @@ import { ApolloServer } from '@apollo/server';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 import bodyParser from 'body-parser';
 import { expressMiddleware } from '@apollo/server/express4';
+import fakeData from './fakeData/index.js'
 
 const app = express();
 const httpServer = http.createServer(app);
 
 const typeDefs = `
+    type Folder {
+        id: String,
+        name : String,
+        createdAt: String,
+        author: Author,
+    }
+
+    type Author{
+        id: String, 
+        name: String,
+    }
+
     type Query {
-        name: String
+        folders: [Folder]
     }
 `;
 const resolvers = {
     Query: {
-        name: () => {
-            return 'hello world'
+        folders: () => {
+            return fakeData.folders
         }
     }
 };
